a9bdcac4b1388f7148d39c1e8ca9295868406842
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-02 18:34:45 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 var salas=new SALAS();
 function SALAS(){
   this.run=function (com){
-    decrypt("
+    decrypt(com);
+  }
+}
+
+function decrypt(com){
+  var coms=split(com,' ');
+  console.log(coms);
+  
+}
+
+function split(str,char){
+  if(str=''||!str||char=''||!char)
+    return null;
+  var out=[];
+  var tmp="";
+  for(var i=0;i<str.length;i++){
+    if(str.charAt(i)!==char){
+      tmp+=str.charAt(i);
+    }else{
+      out.push(tmp);
+      tmp="";
+    }
+  }
+  return out;
+}
