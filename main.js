ac2da94a810d1de228cf83d5cf3f355db71cc491
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-02 18:51:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ function SALAS(){
   this.run=function (com){
     decrypt(com);
   }
+  this.get=function(coms){
+    if(coms[1]="IP_ADDRESS"){
+      return this.getIP()
+    }
+  }
 }
 
 function decrypt(com){
   var coms=split(com,' ');
+  var tmp="";
   console.log(coms);
-  
+  if(coms[0]==="get"){
+    salas.get(coms);
+  }
 }
 
 function split(str,char){
@@ -26,4 +34,50 @@ function split(str,char){
   }
   out.push(tmp);
   return out;
+}
+function getIP(){
+function getUserIP(onNewIP) { 
+  var ipaddress;
+    //  onNewIp - your listener function for new IPs
+    //compatibility for firefox and chrome
+    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var pc = new myPeerConnection({
+        iceServers: []
+    }),
+    noop = function() {},
+    localIPs = {},
+    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    key;
+
+    function iterateIP(ip) {
+        if (!localIPs[ip]) onNewIP(ip);
+        localIPs[ip] = true;
+    }
+
+     //create a bogus data channel
+    pc.createDataChannel("");
+
+    // create offer and set local description
+    pc.createOffer(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+            if (line.indexOf('candidate') < 0) return;
+            line.match(ipRegex).forEach(iterateIP);
+        });
+        
+        pc.setLocalDescription(sdp, noop, noop);
+    }, noop); 
+
+    //listen for candidate events
+    pc.onicecandidate = function(ice) {
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+    };
+}
+
+// Usage
+
+getUserIP(function(ip){
+  ipaddress=ip;
+});
+return ipaddress;
 }
